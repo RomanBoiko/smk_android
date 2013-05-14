4344c64d68364ae7e98b3d482c294a4d385d6fe3
boikoro <roman.boiko@ubs.com>
2013-05-14 20:31:02 +0100

    [initial] Icon and skeleton for prototype

---
+++ src/main/java/com/smarkets/BootstrapActivity.java
@@ -0,0 +1,22 @@
+package com.smarkets;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class BootstrapActivity extends Activity {
+
+	private static String TAG = "smk_android";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.i(TAG, "onCreate");
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle instanceStateToSave) {
+		Log.i(TAG, "saveOnShutdown");
+	}
+}
