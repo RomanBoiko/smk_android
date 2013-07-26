dae5e865365a7d1bb646f8d01a5273742612ebe7
boikoro <roman.boiko@ubs.com>
2013-07-26 06:04:43 +0100

    env connection properties consolidated

--- src/main/java/com/smarkets/android/SmkConfig.java
+++ src/main/java/com/smarkets/android/SmkConfig.java
@@ -3,12 +3,16 @@ package com.smarkets.android;
 public class SmkConfig {
 
 	//==========SANDBOX=============
-//	public final String smkRestApiRoot() { return "https://api.sandbox.smarkets.com"; }
-//	public final String smkStreamingApiHost() { return "api.sandbox.smarkets.com"; }
-	public final String smkRestApiRoot() { return "https://api.smarkets.com"; }
-	public final String smkStreamingApiHost() { return "api.smarkets.com"; }
+	public final String smkRestApiRoot() { return "https://api.sandbox.smarkets.com"; }
+	public final String smkStreamingApiHost() { return "api.sandbox.smarkets.com"; }
 	public final Integer smkStreamingApiPort() { return 3701; }
 	public final Boolean smkStreamingApiSslEnabled() { return true; }
+
+	//==========PROD=============
+//	public final String smkRestApiRoot() { return "https://api.smarkets.com"; }
+//	public final String smkStreamingApiHost() { return "api.smarkets.com"; }
+//	public final Integer smkStreamingApiPort() { return 3701; }
+//	public final Boolean smkStreamingApiSslEnabled() { return true; }
 
 	//==========LOCAL VARGANT=======
 //	public final String smkRestApiRoot = "http://vagrant-dev.corp.smarkets.com:8007";
